c3be72f1d3192c95f867995761e7183fd1f9d0f8
vmd <vuminhduc.vmd.vn@gmail.com>
2026-01-23 11:03:03 +0800

    Complete Level-1

--- src/main/java/Sky.java
+++ src/main/java/Sky.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Sky {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________");
         System.out.println("Hello! I'm Sky");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________");
+            System.out.println("    " + input);
+            System.out.println("____________________________________________");
+        }
+
+        scanner.close();
     }
 }
