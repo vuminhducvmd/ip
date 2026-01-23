2d0716bfdfb8d6914cbec9440e9f361291662271
vmd <vuminhduc.vmd.vn@gmail.com>
2026-01-23 10:58:27 +0800

    Complete Level-0

---
+++ src/main/java/Sky.java
@@ -0,0 +1,10 @@
+public class Sky {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________");
+        System.out.println("Hello! I'm Sky");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________");
+    }
+}
